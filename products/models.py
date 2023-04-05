c032fe4f929ce419aadcbff4549b6f0e9c4e75a4
Zhavlon <zhavlon.khamidov@alatoo.edu.kg>
2023-04-05 16:45:25 +0600

    pagination in products

--- products/models.py
+++ products/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 import uuid
 
+
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField(null=True,blank=True)
+    description = models.TextField(null=True, blank=True)
     featured_image = models.ImageField(default='default.jpg')
     price = models.FloatField(null=True, blank=True, default=0)
-    min_order= models.IntegerField(default=1)
+    min_order = models.IntegerField(default=1)
     stock = models.IntegerField(default=0)
     exp_date = models.DateField(null=True, blank=True)
     category = models.ForeignKey('Category',
@@ -17,26 +18,28 @@ class Product(models.Model):
     # edited = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True)
-    
-    
+
     def __str__(self) -> str:
         return self.title
-    
+
+    class Meta:
+        ordering = ['created']
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True)
-    
+
     def __str__(self) -> str:
         return self.name
-    
-    
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True)
-    
+
     def __str__(self) -> str:
         return self.name
 
--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
 from products.models import Product
 from .forms import ProductForm
 from django.contrib.auth.decorators import login_required
+
 
 # data = [
 #     {'id':1, 'title':'Apple','price':2.3, 'description':'Apple is from Kyrgyzstan'},
@@ -18,45 +20,70 @@ def deleteProduct(request, pk):
     if request.method == "POST":
         product.delete()
         return redirect('products')
-    return render(request, 'products/delete.html',{'product':product})
+    return render(request, 'products/delete.html', {'product': product})
+
 
 @login_required(login_url='/login')
-def createProduct(request : HttpRequest):
+def createProduct(request: HttpRequest):
     form = ProductForm()
     print(request.method)
-    
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('products')
     context = {"form": form}
-    return render(request,'products/product-form.html',context)
+    return render(request, 'products/product-form.html', context)
+
 
 @login_required(login_url='/login')
-def editProduct(request : HttpRequest, pk):
+def editProduct(request: HttpRequest, pk):
     product = Product.objects.get(id=pk)
     form = ProductForm(instance=product)
     print(request.method)
-    
+
     if request.method == 'POST':
-        form = ProductForm(request.POST,instance=product)
+        form = ProductForm(request.POST, instance=product)
         if form.is_valid():
             form.save()
-            return redirect('product',product.id)
+            return redirect('product', product.id)
     context = {"form": form}
-    return render(request,'products/product-form.html',context)
+    return render(request, 'products/product-form.html', context)
+
 
 def category(request, category):
     products = Product.objects.filter(category__name=category)
-    return render(request, 'products/products.html',{'products':products})
+    return render(request, 'products/products.html', {'products': products})
+
 
 def productsHandler(request):
-    pageName = 'All Products'; 
     products = Product.objects.all()
-    authPerson = {'name':'Alisher'}
-    return render(request,'products/products.html',
-                  {'products':products})
+    page = 1
+    if request.GET.get('page'):
+        page = int(request.GET['page'])
+    paginator = Paginator(products, 4)
+    try:
+        products = paginator.page(page)
+    except:
+        products = paginator.page(paginator.num_pages)
+
+    page_range = paginator.page_range
+    page_start = 1
+    page_end = paginator.num_pages
+    if paginator.num_pages > 10:
+        page_start = products.number - 5 if products.number - 5 > 1 else 1
+        page_end = products.number + 5 if products.number + 5 < paginator.num_pages else paginator.num_pages
+        page_range = range(page_start, page_end + 1)
+
+    return render(request, 'products/products.html',
+                  {
+                      'products': products,
+                      'page_range': page_range,
+                      'page_start':page_start,
+                      'page_end':page_end,
+                      'paginator':paginator,
+                  })
 
 
 def productHandler(request, pk):
@@ -66,8 +93,8 @@ def productHandler(request, pk):
     #     if d['id'] == pk:
     #         product = d
     #         break
-            
+
     authPerson = None
-    return render(request, 'products/single-product.html', 
-                  {'page': 'Single Product','auth': authPerson,
+    return render(request, 'products/single-product.html',
+                  {'page': 'Single Product', 'auth': authPerson,
                    'product': product})
